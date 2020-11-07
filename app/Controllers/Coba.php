494f52f7847e029f2e23a8c88992f71bc0f24b66
dimassfm <dimasfaturohman05@gmail.com>
2020-11-07 23:46:06 +0700

    Melengkapi fitur login, logout, register, user activation, forgot password remember me tetapi masih error

---
+++ app/Controllers/Coba.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class Coba extends BaseController
+{
+    public function index()
+    {
+        return view('welcome_message');
+    }
+
+    //--------------------------------------------------------------------
+
+}
---
+++ app/Views/pages/about.php
@@ -0,0 +1,13 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About Me</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae nemo omnis temporibus magni inventore minima aliquid possimus deserunt eligendi harum! Adipisci, nemo? Saepe illum accusamus doloremque quod voluptatibus fuga quibusdam?</p>
+        </div>
+    </div>
+</div>
+<?= $this->endSection(); ?>
